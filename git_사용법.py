90db347b92937a96d519452f71c005cf3a3a9434
tofooci <tofooci@gmail.com>
2022-01-14 16:16:09 +0900

    test

---
+++ git_사용법.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ## git ignore
+
+# ##### 파일 무시 
+# test.txt 
+#
+# ##### 다음과 같은 확장자는 전체 무시 
+# *.text   
+# *.exe   
+# *.zip   
+#
+# ##### 폴더 무시 
+# test/  
